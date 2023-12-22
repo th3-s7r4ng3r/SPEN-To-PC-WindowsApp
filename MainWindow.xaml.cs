541d7f63e10597a61f8da3c8e31c6039d21a8d94
VinuraGallage <th3-s7r4ng3r@users.noreply.github.com>
2023-12-22 21:49:53 +0530

    Add: Communication between Android and PC app

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,13 +18,15 @@ using System.Threading.Tasks;
 
 namespace SPEN_To_PC_WindowsApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        //declaring all variables
+        // Declaring variables for persistent connection
         private TcpListener? tcpListener;
+        private CancellationTokenSource? cancellationSource;
+        private TcpClient? client;
+        private NetworkStream? stream;
+
+        // Other variables
         private const int KEYEVENTF_EXTENDEDKEY = 0x0001;
         private const int KEYEVENTF_KEYUP = 0x0002;
         private const int VK_LEFT = 0x25;  // Left arrow key
@@ -40,6 +42,7 @@ namespace SPEN_To_PC_WindowsApp
             //setting up networking
             DisplayIPAddress();
             InitializeTcpListener();
+            cancellationSource = new CancellationTokenSource();
             StartTcpListener();
         }
 
@@ -90,22 +93,21 @@ namespace SPEN_To_PC_WindowsApp
             tcpListener = new TcpListener(IPAddress.Any, 51515);
         }
 
-        private async void StartTcpListener()
+        private async Task StartTcpListener()
         {
             try
             {
                 // Start listening for incoming connections
                 tcpListener?.Start();
 
-                while (true)
+                while (!cancellationSource.IsCancellationRequested)
                 {
                     try
                     {
-                        // Accept a pending connection
-                        TcpClient client = await tcpListener.AcceptTcpClientAsync();
-
-                        // Process the received data on a separate thread
-                        Task.Run(() => HandleClient(client));
+                        // Accept a single connection and handle it persistently
+                        client = await tcpListener.AcceptTcpClientAsync();
+                        stream = client.GetStream();
+                        await HandleClientPersistently();
                     }
                     catch (Exception ex)
                     {
@@ -120,34 +122,38 @@ namespace SPEN_To_PC_WindowsApp
             }
         }
 
-        private void HandleClient(TcpClient client)
+        private async Task HandleClientPersistently()
         {
             try
             {
-                // Get the network stream
-                NetworkStream stream = client.GetStream();
-
-                // Read the incoming data
-                byte[] buffer = new byte[1024];
-                int bytesRead = stream.Read(buffer, 0, buffer.Length);
-
-                // Convert the received data to a string
-                string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-
-                // Process the received data and perform actions
-                // Update UI and simulate arrow key presses based on received data
-                Dispatcher.Invoke(() =>
+                while (!cancellationSource.Token.IsCancellationRequested)
                 {
-                    ProcessReceivedData(receivedData);
-                });
+                    // Read incoming data continuously
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
 
-                // Close the client connection
-                client.Close();
+                    // Process received data
+                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    if (receivedData == "connection reset") // Check for a special message
+                    {
+                        Console.WriteLine("Desktop app closed. Sending close signal.");
+                        break;
+                    }
+                    ProcessReceivedData(receivedData);
+                }
             }
             catch (Exception ex)
             {
-                // Handle exceptions (e.g., if there's an issue with the client)
+                // Handle exceptions (e.g., if the client disconnects)
                 Console.WriteLine($"Error handling client: {ex.Message}");
+            }
+            finally
+            {
+                // Cleanup resources
+                stream?.Dispose();
+                client?.Close();
+                // Inform the mobile app that the connection is closed
+                SendCloseSignalToMobileApp();
             }
         }
 
@@ -183,9 +189,41 @@ namespace SPEN_To_PC_WindowsApp
         }
 
 
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                // Cleanup resources on window closing
+                cancellationSource?.Cancel();
 
+                if (tcpListener != null && tcpListener.Server.IsBound)
+                {
+                    // Stop the listener if it's still running
+                    tcpListener.Stop();
+                }
 
+                // Wait for HandleClientPersistently to complete before closing the window
+                Task.WaitAll(HandleClientPersistently());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during window closing: {ex.Message}");
+            }
+        }
 
+        private void SendCloseSignalToMobileApp()
+        {
+            try
+            {
+                // Inform the mobile app that the connection is closed by sending a special message
+                byte[] closeSignal = Encoding.UTF8.GetBytes("connection reset");
+                stream.Write(closeSignal, 0, closeSignal.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending close signal: {ex.Message}");
+            }
+        }
 
     }
 }
