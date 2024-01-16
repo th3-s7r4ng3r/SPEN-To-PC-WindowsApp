d6b607ee52246f51561cb803fdf168362931e07e
th3-s7r4ng3r <gvinura@gmail.com>
2024-01-16 21:55:20 +0530

    Ref: Fix a bug in message popup

--- obj/Debug/net8.0-windows/SPEN-To-PC-WindowsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SPEN-To-PC-WindowsApp.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPEN To PC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPEN To PC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPEN To PC")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
